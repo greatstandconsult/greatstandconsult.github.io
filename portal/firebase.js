191d5724281c5411f4475bf3da67c531e35c46a0
greatstandconsult <greatstandtutorial22@gmail.com>
2026-09-07 08:59:52 +0100

    Add Firebase initialization and configuration

---
+++ portal/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX_ZC18wviACEbBgwTLBQiD95HVZg2oxDY",
+  authDomain: "great-stand-consult.firebaseapp.com",
+  projectId: "great-stand-consult",
+  storageBucket: "great-stand-consult.firebasestorage.app",
+  messagingSenderId: "759290092567",
+  appId: "1:759290092567:web:4e873f1ad166cb54d11052",
+  measurementId: "G-BPTN65KHK7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
