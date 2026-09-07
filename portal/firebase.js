7c25f6355c97ef438120c4bcfb8206b563993bb1
greatstandconsult <greatstandtutorial22@gmail.com>
2026-09-07 09:26:42 +0100

    Update Firebase imports and add analytics

--- portal/firebase.js
+++ portal/firebase.js
@@ -1,9 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCX_ZC18wviACEbBgwTLBQiD95HVZg2oxDY",
+  apiKey: "AIzaSyCX_ZC18wviACEbGwTLBQiD95HVZg2oxDY",
   authDomain: "great-stand-consult.firebaseapp.com",
   projectId: "great-stand-consult",
   storageBucket: "great-stand-consult.firebasestorage.app",
@@ -12,7 +16,6 @@ const firebaseConfig = {
   measurementId: "G-BPTN65KHK7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
