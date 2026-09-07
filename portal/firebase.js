296926ef65792ee8ebabb05f5856b8cdadd3f664
greatstandconsult <greatstandtutorial22@gmail.com>
2026-09-07 09:53:05 +0100

    Add Firebase config and initialize analytics
    Added Firebase configuration values and initialized analytics.

--- portal/firebase.js
+++ portal/firebase.js
@@ -3,10 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // PUT YOUR CURRENT FIREBASE CONFIG VALUES HERE
+  apiKey: "AIzaSyCX_ZC18wviACEbGwTLBQiD95HVZg2oxDY",
+  authDomain: "great-stand-consult.firebaseapp.com",
+  projectId: "great-stand-consult",
+  storageBucket: "great-stand-consult.firebasestorage.app",
+  messagingSenderId: "759290092567",
+  appId: "1:759290092567:web:4e873f1ad166cb54d11052",
+  measurementId: "G-BPTN65KHK7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
