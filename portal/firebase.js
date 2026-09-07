19993197c3e73332f56500ec1a111375aa20e9ba
greatstandconsult <greatstandtutorial22@gmail.com>
2026-09-07 09:36:29 +0100

    Refactor Firebase imports and initialize Auth/Firestore
    Updated Firebase imports to use CDN links and added Firestore and Auth initialization.

--- portal/firebase.js
+++ portal/firebase.js
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCX_ZC18wviACEbGwTLBQiD95HVZg2oxDY",
-  authDomain: "great-stand-consult.firebaseapp.com",
-  projectId: "great-stand-consult",
-  storageBucket: "great-stand-consult.firebasestorage.app",
-  messagingSenderId: "759290092567",
-  appId: "1:759290092567:web:4e873f1ad166cb54d11052",
-  measurementId: "G-BPTN65KHK7"
+  // PUT YOUR CURRENT FIREBASE CONFIG VALUES HERE
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
